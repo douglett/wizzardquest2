a4715b1a4b01b455a3d7cab497463a31e4184b55
Doug Inman <doug@r3spin.co.uk>
2026-04-24 17:00:56 +0100

    tile movement 1

--- main.go
+++ main.go
@@ -20,21 +20,44 @@ func main() {
 	defer screen.destroy()
 	gmap.load("assets/world.tmx")
 
-	var posx, posy float32
+	// var posx, posy float32
+	move := -1
 
 	for !ray.WindowShouldClose() {
 		if ray.IsKeyPressed(ray.KeySpace) {
 			ray.PlaySound(screen.sound)
 		}
 
-		if ray.IsKeyDown(ray.KeyUp)    { posy-- }
-		if ray.IsKeyDown(ray.KeyDown)  { posy++ }
-		if ray.IsKeyDown(ray.KeyLeft)  { posx-- }
-		if ray.IsKeyDown(ray.KeyRight) { posx++ }
+		// if ray.IsKeyDown(ray.KeyUp)    { posy-- }
+		// if ray.IsKeyDown(ray.KeyDown)  { posy++ }
+		// if ray.IsKeyDown(ray.KeyLeft)  { posx-- }
+		// if ray.IsKeyDown(ray.KeyRight) { posx++ }
+
+		switch move {
+		case 0:
+			player.oy--
+			if player.oy <= -16 { player.oy = 0; player.y--; move = -1 }
+		case 1:
+			player.ox++
+			if player.ox >= 16 { player.ox = 0; player.x++; move = -1 }
+		case 2:
+			player.oy++
+			if player.oy >= 16 { player.oy = 0; player.y++; move = -1 }
+		case 3:
+			player.ox--
+			if player.ox <= -16 { player.ox = 0; player.x--; move = -1 }
+		case -1:
+			if ray.IsKeyDown(ray.KeyUp)    { move = 0 }
+			if ray.IsKeyDown(ray.KeyRight) { move = 1 }
+			if ray.IsKeyDown(ray.KeyDown)  { move = 2 }
+			if ray.IsKeyDown(ray.KeyLeft)  { move = 3 }
+		}
 
 		screen.begin()
-			screen.offx = -posx
-			screen.offy = -posy
+			// player.ox = posx
+			// player.oy = posy
+			screen.offx = float32((screen.width-screen.tsize)/2) - (float32(player.x*screen.tsize) + player.ox)
+			screen.offy = float32((screen.height-screen.tsize)/2) - (float32(player.y*screen.tsize) + player.oy)
 			gmap.show(0, 0)
 			screen.blitt(screen.tileset, player.tile, float32(player.x*screen.tsize)+player.ox, float32(player.y*screen.tsize)+player.oy)
 		screen.flip()
