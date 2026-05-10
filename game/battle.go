efe4b8cad58f9e3c73272a6ddec08765d9b31095
Doug Inman <doug@r3spin.co.uk>
2026-05-10 23:16:49 +0100

    added enemy attack pattern

--- game/battle.go
+++ game/battle.go
@@ -1,14 +1,16 @@
 package game
 import "fmt"
 import "math"
+import "math/rand"
 import ray "github.com/gen2brain/raylib-go/raylib"
 
 type Battle struct {
-	monsters  ray.Texture2D
-	spells    ray.Texture2D
-	frame     int
-	hand      int
-	actions   [4]string
+	monsters   ray.Texture2D
+	spells     ray.Texture2D
+	frame      int
+	hand       int
+	actions    [4]string
+	offx, offy int
 }
 
 var dialogbox = GMapFrag{
@@ -45,12 +47,18 @@ func (bt *Battle) mainloop() {
 			case ray.IsKeyPressed(ray.KeyEnter):
 				switch bt.actions[bt.hand] {
 					case "":
-					case "fireball":  bt.cast()
+					case "fireball":
+						bt.cast()
+						bt.enemyattack()
 					// case "run":
 					default:  fmt.Printf("unknown selection '%s'\n", bt.actions[bt.hand])
 				}
 		}
-		bt.paintall()
+		// paint screen
+		screen.begin()
+		bt.paint()
+		bt.painthand()
+		screen.flip()
 		bt.frame++
 	}
 }
@@ -84,6 +92,27 @@ func (bt *Battle) cast() {
 	}
 }
 
+func (bt *Battle) enemyattack() {
+	// shake screen
+	dist := 2
+	for i := 0; i < 50; i++ {
+		if i % 3 == 0 {
+			bt.offx = (rand.Intn(2) - 1) * dist
+			bt.offy = (rand.Intn(2) - 1) * dist
+		}
+		bt.paintall()
+	}
+	bt.offx, bt.offy = 0, 0
+
+	// show damage
+	for i := 0; i < 100; i++ {
+		screen.begin()
+		bt.paint()
+		screen.text("You take 10\ndamage!", 7, 3)
+		screen.flip()
+	}
+}
+
 func (bt *Battle) paintall() {
 	screen.begin()
 	bt.paint()
@@ -96,6 +125,8 @@ func (bt *Battle) paint() {
 	// battle screen
 	screen.offsetx, screen.offsety = (screen.width-battlebox.width())/2, 20
 	battlebox.border(10)
+	screen.offsetx += bt.offx
+	screen.offsety += bt.offy
 	battlebox.show()
 	sprindex := (bt.frame / 30) % 3
 	screen.blitt(bt.monsters, sprindex, (battlebox.width()-screen.tsize)/2, (battlebox.height()-screen.tsize)/2)
@@ -104,7 +135,10 @@ func (bt *Battle) paint() {
 	screen.offsetx, screen.offsety = (screen.width-dialogbox.width())/2, 120
 	dialogbox.border(2)
 	dialogbox.show()
+}
 
+func (bt *Battle) painthand() {
+	// paint hand dialog
 	screen.text(bt.actions[0], 14, 4)
 	screen.text(bt.actions[1], 14, 16)
 	screen.text(bt.actions[2], 64, 4)
